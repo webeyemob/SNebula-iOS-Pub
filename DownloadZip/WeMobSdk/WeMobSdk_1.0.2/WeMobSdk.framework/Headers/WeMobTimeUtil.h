d8de905e0621b50a7a75d4e85c91b13f58c4147a
hang.wang <847972062@qq.com>
2019-07-03 18:37:43 +0800

    add 1.0.2

---
+++ DownloadZip/WeMobSdk/WeMobSdk_1.0.2/WeMobSdk.framework/Headers/WeMobTimeUtil.h
@@ -0,0 +1,12 @@
+//
+//  WeMobTimeUtil.h
+//  WeMobSdk
+//
+//  Created by 王航 on 2019/6/17.
+//
+
+@interface WeMobTimeUtil : NSObject
+
++(long long)getDateTimeMilliSeconds;
+
+@end
---
+++ WeMobSdk/1.0.2/WeMobSdk.framework/Headers/WeMobWeCreativeNative.h
@@ -0,0 +1,17 @@
+//
+//  WeMobWeCreativeNative.h
+//  WeMobSdk
+//
+//  Created by 汤正 on 2019/6/27.
+//  Copyright © 2019 王航. All rights reserved.
+//
+
+#import "WeMobCustomNative.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WeMobWeCreativeNative : WeMobCustomNative
+
+@end
+
+NS_ASSUME_NONNULL_END
