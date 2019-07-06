f9bf3aabceb99b1e53cacef1071f85581e932fea
hang.wang <847972062@qq.com>
2019-07-06 22:46:24 +0800

    release nend 5.1.1.2

---
+++ WeMobMediation_Nend/5.1.1.2/WeMobMediation_Nend.framework/Headers/WeMobNendNormalInterstitial.h
@@ -0,0 +1,14 @@
+//
+//  WeMobNendNormalInterstitial.h
+//  WeMobMediation_Nend
+//
+//  Created by wanghang on 2019/6/27.
+//  Copyright © 2019年 WeSdk. All rights reserved.
+//
+
+#import "WeMobBaseInterstitial.h"
+#import <NendAd/NendAd.h>
+
+@interface WeMobNendNormalInterstitial : WeMobBaseInterstitial<NADInterstitialDelegate>
+
+@end
