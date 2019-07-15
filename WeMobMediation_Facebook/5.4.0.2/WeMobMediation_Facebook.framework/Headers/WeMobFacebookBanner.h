92f70ecb57c8fbbd47080e9c8b331c4fd841844a
hang.wang <847972062@qq.com>
2019-07-15 23:16:46 +0800

    release facebook 5.4.0.2

---
+++ WeMobMediation_Facebook/5.4.0.2/WeMobMediation_Facebook.framework/Headers/WeMobFacebookBanner.h
@@ -0,0 +1,11 @@
+//
+//  WeMobFacebookBanner.h
+//  WeMobMediation_Facebook
+//
+
+#import "WeMobCustomBanner.h"
+#import <FBAudienceNetwork/FBAudienceNetwork.h>
+
+@interface WeMobFacebookBanner : WeMobCustomBanner<FBAdViewDelegate>
+
+@end
