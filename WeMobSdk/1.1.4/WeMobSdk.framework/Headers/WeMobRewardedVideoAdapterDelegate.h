00e9945618d2d27253cbdbdef041fc11417440a9
hang.wang <847972062@qq.com>
2019-07-16 10:08:54 +0800

    release 1.1.4

---
+++ WeMobSdk/1.1.4/WeMobSdk.framework/Headers/WeMobRewardedVideoAdapterDelegate.h
@@ -0,0 +1,24 @@
+//
+//  WeMobRewardedVideoAdapterDelegate.h
+//  WeMobSdk
+//
+//  Created by wanghang on 2019/6/18.
+//
+
+#import <Foundation/Foundation.h>
+#import "WeMobAdError.h"
+#import "WeMobRewardItem.h"
+
+@protocol WeMobRewardedVideoAdapterDelegate<NSObject>
+
+- (void)rewardedVideoDidReceiveAd;
+- (void)rewardedVideoDidOpen;
+- (void)rewardedVideoWillLeaveApplication;
+- (void)rewardedVideoDidClose;
+- (void)rewardedVideoDidFailToReceiveAdWithError:(WeMobAdError *)adError;
+- (void)rewardedVideoDidStart;
+- (void)rewardedVideoDidComplete;
+- (void)rewardedVideoDidReward:(WeMobRewardItem *)item;
+- (void)rewardedVideoDidFailedToReward;
+
+@end
